a45dc59c235d310a18c5a607b8ea731dbcff0edb
SebWeb <140866849+apps-seb@users.noreply.github.com>
2026-02-19 04:23:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -371,19 +371,21 @@ document.addEventListener('DOMContentLoaded', () => {
     if (walletCards.length > 0) {
         function setActiveWalletCard(activeIndex) {
             walletCards.forEach((c, i) => {
-                c.classList.remove('active', 'shift-down');
+                c.classList.remove('active', 'move-out-up', 'move-out-down');
 
                 if (i === activeIndex) {
                     c.classList.add('active');
-                } else if (i > activeIndex) {
-                    c.classList.add('shift-down');
+                } else if (i < activeIndex) {
+                    c.classList.add('move-out-up');
+                } else {
+                    c.classList.add('move-out-down');
                 }
             });
         }
 
         function resetWallet() {
             walletCards.forEach(c => {
-                c.classList.remove('active', 'shift-down');
+                c.classList.remove('active', 'move-out-up', 'move-out-down');
             });
         }
 
